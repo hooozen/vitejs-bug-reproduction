016611d2e52f6be97362b4c809ee8f96e58c9506
Hozen <Hozen@live.com>
2021-06-03 17:30:50 +0800

    home statistics

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,25 @@
+import ECharts from 'vue-echarts'
+import { use } from "echarts/core";
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+import { CanvasRenderer } from 'echarts/renderers'
+import { LineChart, BarChart, PieChart } from 'echarts/charts'
+import { GridComponent, TooltipComponent, LegendComponent, TitleComponent, ToolboxComponent } from 'echarts/components'
+
+use([
+  CanvasRenderer,
+  BarChart,
+  PieChart,
+  LineChart,
+  ToolboxComponent,
+  TitleComponent,
+  LegendComponent,
+  GridComponent,
+  TooltipComponent
+]);
 
 import 'dayjs/locale/zh-cn'
 import locale from 'element-plus/lib/locale/lang/zh-cn'
@@ -18,4 +36,7 @@ createApp(App)
   .use(store)
   .use(router)
   .use(ElementPlus, { locale })
+  .component('v-chart', ECharts)
   .mount('#app')
+
+
